c4fc90cba7fefb0c0b2d532f070daace476a327f
jerry <jm90_yang@163.com>
2019-04-20 14:21:04 +0800

    add logback.xml

--- it-shop-member/src/main/java/com/it/serviceImpl/DemoApiServiceImpl.java
+++ it-shop-member/src/main/java/com/it/serviceImpl/DemoApiServiceImpl.java
@@ -3,6 +3,7 @@ package com.it.serviceImpl;
 import com.it.redis.RedisUtil;
 import com.it.response.BaseApiService;
 import com.it.service.DemoApiService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.Map;
  * @ Description：实现类
  * @ throws
  */
+@Slf4j
 @RestController
 public class DemoApiServiceImpl extends BaseApiService implements DemoApiService {
 
@@ -35,6 +37,10 @@ public class DemoApiServiceImpl extends BaseApiService implements DemoApiService
     @Override
     public Map<String, Object> getKey(String key) {
         String str = redisUtil.get(key);
+        log.info("测试日志打印");
+        if (null == str) {
+            return setResultError("未从Redis中获取value值");
+        }
         return setResultSuccessData(str);
     }
 }
