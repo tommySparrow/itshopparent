2cf549304a68b32484e0fcada09457a92e37aff1
jerry <jm90_yang@163.com>
2019-04-20 11:06:37 +0800

    integration  Redis

--- it-shop-api/it-shop-member-api/src/main/java/com/it/service/DemoApiService.java
+++ it-shop-api/it-shop-member-api/src/main/java/com/it/service/DemoApiService.java
@@ -1,5 +1,6 @@
 package com.it.service;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
@@ -7,6 +8,19 @@ import java.util.Map;
 @RequestMapping("/demo")
 public interface DemoApiService {
 
+    /**
+     * @ Description 测试demo
+     */
     @RequestMapping("/demo")
-    Map<String,Object> demo();
+    Map<String, Object> demo();
+    /**
+     * @ Description 测试Redis存储
+     */
+    @GetMapping("/setKey")
+    Map<String, Object> setKey(String key, String value);
+    /**
+     * @ Description 测试Redis获取
+     */
+    @GetMapping("/getKey")
+    Map<String, Object> getKey(String key);
 }
--- it-shop-member/src/main/java/com/it/serviceImpl/DemoApiServiceImpl.java
+++ it-shop-member/src/main/java/com/it/serviceImpl/DemoApiServiceImpl.java
@@ -1,7 +1,9 @@
 package com.it.serviceImpl;
 
+import com.it.redis.RedisUtil;
 import com.it.response.BaseApiService;
 import com.it.service.DemoApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -15,9 +17,24 @@ import java.util.Map;
 @RestController
 public class DemoApiServiceImpl extends BaseApiService implements DemoApiService {
 
+    @Autowired
+    private RedisUtil redisUtil;
+
     @Override
     public Map<String, Object> demo() {
 
         return setResultSuccess();//成功返回
+    }
+
+    @Override
+    public Map<String, Object> setKey(String key, String value) {
+        redisUtil.setString(key, value);
+        return setResultSuccess();
+    }
+
+    @Override
+    public Map<String, Object> getKey(String key) {
+        String str = redisUtil.get(key);
+        return setResultSuccessData(str);
     }
 }
