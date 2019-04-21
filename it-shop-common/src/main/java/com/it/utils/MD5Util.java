c9bac70451d61293e30364cb69e88ed8c281f42f
jerry <jm90_yang@163.com>
2019-04-21 09:36:17 +0800

    add utils

---
+++ it-shop-common/src/main/java/com/it/utils/MD5Util.java
@@ -0,0 +1,43 @@
+package com.it.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2019/4/21
+ * @ Description：MD5工具类
+ * @ throws
+ */
+public class MD5Util {
+
+    private static String MD5(String pwd) {
+        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        try {
+            byte[] btInput = pwd.getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char[] str = new char[j * 2];
+            int k = 0;
+            for (byte byte0 : md) {
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MD5Util.MD5("123456"));
+
+
+    }
+}
